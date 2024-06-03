4ac65ddc37d5931a331e552b27b4be97b4443c85
Mohammad Shahidul Alam <shahidulalam234@gmail.com>
2024-06-03 19:18:13 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ const App = () => (
     Nav
     <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
     <section className="padding">popularProduct</section>
+    <section className="padding">SuperQuality</section>
+    <section className="padding">Services</section>
+    <section className="padding">SpecialOffer</section>
+    <section className="padding">CustomerReview</section>
+    <section className="padding">Subscribe</section>
+    <section className="padding">Footer</section>
   </main>
 );
 
