b49a4f1cd05456595d2349ecd498f22bcae9dbb1
Mohammad Shahidul Alam <shahidulalam234@gmail.com>
2024-06-03 16:14:26 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
+    <section className="padding">popularProduct</section>
+  </main>
+);
+
+export default App;
