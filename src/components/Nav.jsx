6b4a741a2beabe5cc5d07f21220040801f611a8f
Mohammad Shahidul Alam <shahidulalam234@gmail.com>
2024-06-07 01:52:18 -0400

    setting Icon and navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+const Nav = () => {
+  return (
+    <header>
+        <nav>
+            <a href="/">
+                <img src="" alt="" />
+            </a>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
