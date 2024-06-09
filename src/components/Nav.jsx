af3c49ef21fe9ec7aceef7f07ddee2d51ef898b7
Mohammad Shahidul Alam <shahidulalam234@gmail.com>
2024-06-08 23:23:31 -0400

    updated nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,22 @@
+import { headerLogo } from "../assets/images";
+import { navLinks } from "../constants";
 const Nav = () => {
   return (
-    <header>
-        <nav>
-            <a href="/">
-                <img src="" alt="" />
-            </a>
-        </nav>
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={130} height={29} />
+        </a>
+        <ul  className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+            {navLinks.map((item)=>(
+                <li key={item.label}>
+                    <a href={item.href} className="fonts-montserrat leading-normal text-lg text-slate-800">{item.label}</a>
+                </li>
+            ))}
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
