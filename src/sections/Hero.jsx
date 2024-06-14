fac058cea747afe03c60d10890b2abaafa84d5d2
Mohammad Shahidul Alam <shahidulalam234@gmail.com>
2024-06-13 22:36:16 -0400

    completing upper section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,37 @@
+import { arrowRight } from "../assets/icons";
+import Button from "../components/Button";
+import { statistics } from "../constants";
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 ">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collections
+        </p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The new Arrival</span>
+          <br />
+          <span className=" text-coral-red inline-block mt-3">Nike</span> shoes
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 py-10">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life
+        </p>
+        <Button label="Shop Now" iconURL={arrowRight} />
+        <div className="flex justify-starts items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p>{stat.value}</p>
+              <p>{stat.label}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
